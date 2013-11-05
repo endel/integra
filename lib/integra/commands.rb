58003b28b9cf57d42abca23477271dfd0a3058c0
Endel Dreyer <endel.dreyer@gmail.com>
2013-11-05 15:52:43 -0200

    generate reports inside features directory

--- lib/integra/commands.rb
+++ lib/integra/commands.rb
@@ -25,17 +25,19 @@ module Integra
           data.gsub!('{app_host}', Integra.config.app_host.to_s)
           f.write(data)
         end
-        Integra.logger.action('create', "./.#{file}")
+        Integra.logger.action('create', ".#{file}")
       end
 
       # Create 'features' and 'support' directories
       FileUtils.mkdir_p(File.expand_path('./features/support/'))
+      FileUtils.mkdir_p(File.expand_path('./features/reports/'))
+      Integra.logger.action('create', 'features/reports/')
 
       # Sample feature
       FileUtils.cp(Integra.gem_libdir + "/template/i18n/sample.#{Integra.config.lang}.feature", File.expand_path('./features/sample.feature'))
-      Integra.logger.action('create', './features/sample.feature')
+      Integra.logger.action('create', 'features/sample.feature')
 
-      support_dest = './features/support/'
+      support_dest = 'features/support/'
       Dir.glob(Integra.gem_libdir + "/template/features/support/*.rb").each do |source|
         unless File.exists?(File.expand_path("#{support_dest}#{File.basename(source)}"))
           FileUtils.cp(source, File.expand_path(support_dest))
