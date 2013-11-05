32870440e52a010cb87cdd67e85cdba22f0edc51
Endel Dreyer <endel.dreyer@gmail.com>
2013-11-05 15:20:20 -0200

    support selenium driver

--- lib/integra/drivers/poltergeist.rb
+++ lib/integra/drivers/poltergeist.rb
@@ -3,7 +3,7 @@ require 'capybara/poltergeist'
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app, {
     :timeout => 35, # seconds to communicate with phantomjs
-    :phantomjs_logger => Logger.new('tmp/console.log'),
+    :phantomjs_logger => Logger.new($stdout),
     :phantomjs_options => [
       '--load-images=no', '--ignore-ssl-errors=yes', '--local-to-remote-url-access=yes'
     ],
